d3cf0eed3337c233b492c2ab164c15845d090b47
dmytro <dziumanenkod@gmail.com>
2025-03-28 09:29:20 +0000

    Question 8 update

--- src/main/java/org/example/Utils/JsonConverter.java
+++ src/main/java/org/example/Utils/JsonConverter.java
@@ -1,11 +1,5 @@
 package org.example.Utils;
 
-import org.example.DAOs.MySqlBookingDao;
-import org.example.DAOs.MySqlCustomerDao;
-import org.example.DAOs.MySqlTableDao;
-import org.example.DTOs.Booking;
-import org.example.DTOs.Customer;
-import org.example.DTOs.RestaurantTable;
 import org.example.Exception.DaoException;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,21 +11,7 @@ public class JsonConverter {
         return new JSONArray(entities);
     }
 
-    public static JSONObject TableEntityToJson(int ID) throws DaoException {
-        MySqlTableDao tableDao = new MySqlTableDao();
-        RestaurantTable table = tableDao.getEntityById(ID);
-        return new JSONObject(table);
-    }
-
-    public static JSONObject CustomerEntityToJson(int ID) throws DaoException {
-        MySqlCustomerDao customerDao = new MySqlCustomerDao();
-        Customer customer = customerDao.getEntityById(ID);
-        return new JSONObject(customer);
-    }
-
-    public static JSONObject BookingEntityToJson(int ID) throws DaoException {
-        MySqlBookingDao bookingDao = new MySqlBookingDao();
-        Booking booking = bookingDao.getEntityById(ID);
-        return new JSONObject(booking);
+    public static <T> JSONObject TableEntityToJson(T object) throws DaoException {
+        return new JSONObject(object);
     }
 }
